e1dc8aafb51997a23161bc54687553dde8e32726
Albin Groen <albin.groen@gmail.com>
2024-12-03 08:05:07 +0100

    infra: add github workflow for building and releasing

--- src/main/index.ts
+++ src/main/index.ts
@@ -71,7 +71,7 @@ function createWindow(): void {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('me.abgn')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
