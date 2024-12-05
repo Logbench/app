7918692b8c25f7dc99d46d02725e1f2d4d39d651
Albin Groen <albin.groen@gmail.com>
2024-12-05 16:56:59 +0100

    infra: set up electron forge

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,9 @@
 
 interface ImportMetaEnv {
   readonly VITE_WORKER_URL: string
+  readonly APPLE_API_KEY: string
+  readonly APPLE_API_KEY_ID: string
+  readonly APPLE_API_ISSUER: string
 }
 
 interface ImportMeta {
